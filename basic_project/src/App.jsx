fa496c1d10ccd48d225017df3c7f5b7784f28c1b
Pablojonh6550 <pablojonh6550@gmail.com>
2023-01-16 09:27:11 -0300

    feat: reorganização da API

--- basic_project/src/App.jsx
+++ basic_project/src/App.jsx
@@ -1,29 +1,29 @@
-import { Component } from 'react';
-import './App.css';
+import { Component } from "react";
+import { loadPosts } from "./utils/load-posts";
+import "./App.css";
+import { Posts } from "./components/Posts";
 
 class App extends Component {
   state = {
-    posts: []
+    posts: [],
   };
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(posts => this.setState({ posts }))
+  async componentDidMount() {
+    await this.loadPosts();
   }
+
+  loadPosts = async () => {
+    const postsAndPhotos = await loadPosts();
+    this.setState({ posts: postsAndPhotos });
+  };
 
   render() {
     const { posts } = this.state;
 
     return (
-      <div className="App">
-        {posts.map(post => (
-          <div key={post.id}>
-            <h1>{post.title}</h1>
-            <p>{post.body}</p>
-          </div>
-        ))}
-      </div>
+      <section className="container">
+        <Posts posts={posts} />
+      </section>
     );
   }
 }
---
+++ basic_project/src/components/Posts/index.jsx
@@ -0,0 +1,9 @@
+import { PostCard } from "../PostCard/index";
+
+export const Posts = ({ posts }) => (
+  <div className="posts">
+    {posts.map((post) => (
+      <PostCard key={post.id} post={post} />
+    ))}
+  </div>
+);
